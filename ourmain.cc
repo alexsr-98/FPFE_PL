aeda7de42248f2392c76d6c4bb1c0c4151df2061
alexsr-98 <alexsoto2011@gmail.com>
2021-03-04 11:51:23 +0100

    First day: materials

--- ourmain.cc
+++ ourmain.cc
@@ -18,13 +18,25 @@ int main() {
   const G4String matCName = "simple";
   G4NistManager::Instance()->ListMaterials(matCName);
   
-  const G4String matName = "G4_Si_askjgd asdgh";
+  const G4String matName = "G4_Si";
   const G4Material* theSi = G4NistManager::Instance()->FindOrBuildMaterial(matName);
   if (theSi != nullptr) {
     G4cout << theSi << G4endl;
   } else {
     G4cout << " nothing: " << matName << G4endl;
   }
+  
+  G4Element* elC = new G4Element(name="Carbon", symbol="C", z=6., a=12.01*g/mole);
+  
+//  // create the isotopes: iz = number of protons and n=number of nucleons 
+//  G4Isotope* U5 = new G4Isotope(name="U235", iz=92, n=235); 
+//  G4Isotope* U8 = new G4Isotope(name="U238", iz=92, n=238); 
+//  
+//  // create the element and build up by adding the isotopes with their abundance:  
+//  G4Element* elU = new G4Element(name="Enriched Uranium", symbol="U", numisotopes=2); 
+//  elU->AddIsotope(U5,abundance=90.*perCent); 
+//  elU->AddIsotope(U8,abundance=10.*perCent); 
+  
   
   return 0;
 }
